733aa06ae9c8fc2b338ea6c9d624934fee2957c9
Minh Luan Le <leminhluan4244@gmail.com>
2021-04-21 17:03:09 +0700

    Update

--- resources/views/pages/category/search.blade.php
+++ resources/views/pages/category/search.blade.php
@@ -4,12 +4,12 @@
             <div class="col-md-8">
                 <form class="">
                     <div class="row">
-                        <div class="col-md-6">
+                        <div class="col-md-9">
                             <div class="form-group">
                                 <input type="text" class="form-control" id="category" name="category" value="{{old('category', '')}}" placeholder="Tiêu đề">
                             </div>
                         </div>
-                        <div class="col-md-6 text-right">
+                        <div class="col-md-3 text-right">
                             <button type="submit" class="btn btn-sm btn-success px-4 btn-square waves-effect waves-light">
                                 <i class="fas fa-search"></i>
                             </button>
--- resources/views/pages/document/list.blade.php
+++ resources/views/pages/document/list.blade.php
@@ -6,15 +6,15 @@
                 <div class="table-responsive">
                     <table class="table mb-0 table-centered">
                         <thead>
-                        <tr>
-                            <th><i class="fas fa-key"></i></th>
-                            <th><i class="far fa-bookmark"></i></th>
-                            <th><i class="fas fa-link"></i></th>
-                            <th><i class="fas fa-list"></i></th>
-                            <th><i class="fas fa-tags"></i></th>
-                            <th><i class="fab fa-affiliatetheme"></i></th>
-                            <th class="text-center"><i class="fas fa-ellipsis-v"></i></th>
-                        </tr>
+                            <tr>
+                                <th><i class="fas fa-key"></i></th>
+                                <th><i class="far fa-bookmark"></i></th>
+                                <th><i class="fas fa-list"></i></th>
+                                <th><i class="fas fa-tags"></i></th>
+                                <th><i class="fab fa-affiliatetheme"></i></th>
+                                <th><i class="fas fa-link"></i></th>
+                                <th class="text-center"><i class="fas fa-ellipsis-v"></i></th>
+                            </tr>
                         </thead>
                         <tbody>
                         @if(0 != sizeof($data['documents']))
@@ -22,11 +22,6 @@
                             <tr>
                                 <td id="td_id_{{ $document->id }}">{{ $document->id }}</td>
                                 <td id="td_name_{{ $document->id }}">{{ $document->name }}</td>
-                                <td>
-                                    <a id="td_link_{{ $document->id }}" href="{{ $document->link }}">
-                                        <i class="fas fa-external-link-alt"></i>
-                                    </a>
-                                </td>
                                 <td>
                                     <span id="td_category_id_{{ $document->id }}" class="d-none">{{ $document->category_id}}</span>
                                     <img src="{{URL::to('/')}}/assets/images/logo.png" alt="" class="rounded-circle thumb-xs mr-1">
@@ -42,6 +37,11 @@
                                     @endforeach
                                 </td>
                                 <td id="td_type_{{ $document->id }}">{{ $document->type }}</td>
+                                <td>
+                                    <a id="td_link_{{ $document->id }}" href="{{ $document->link }}">
+                                        <i class="fas fa-external-link-alt"></i>
+                                    </a>
+                                </td>
                                 <td class="text-center">
                                     <span onclick="setDocumentEdit('{{$document->id}}')" class="badge badge-soft-secondary"  data-toggle="modal" data-target="#editModal" data-animation="fade" >
                                         <i class="las la-pen text-primary font-18"></i>
